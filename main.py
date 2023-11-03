6a5f498c85c9c0c618e0bdf1d7fba787ea93a612
Dr. J. M. Ashfaque, MInstP, MAAT, AATQB <39216339+UKVeteran@users.noreply.github.com>
2023-11-03 12:45:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import customtkinter
 from customtkinter import filedialog
 import tkinter as tk
-from PIL import Image, ImageTk
 import fitz  # PyMuPDF
 from transformers import pipeline
 
@@ -9,7 +8,7 @@ customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
-root.geometry("1200x500")
+root.geometry("1400x700")  # Increased the width and height
 
 # Create a pipeline for question-answering using a pre-trained model
 qa_pipeline = pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
@@ -19,15 +18,12 @@ viewer_frame = customtkinter.CTkFrame(master=root)
 viewer_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
 # Create a Text widget to display PDF content on the left
-pdf_text = tk.Text(viewer_frame, wrap="none")
+pdf_text = tk.Text(viewer_frame, wrap="none", width=80, height=20)  # Increased width
 pdf_text.grid(row=0, column=0, pady=10, padx=10, sticky="nsew")
 
 # Create a frame for the title and buttons on the right
 button_frame = customtkinter.CTkFrame(master=root)
 button_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
-
-# Store rendered images from the PDF
-pdf_images = []
 
 def upload_pdf():
     filepath = filedialog.askopenfilename(
@@ -37,36 +33,19 @@ def upload_pdf():
     if filepath:
         print(f"File path: {filepath}")
 
-        # Open the PDF file and extract text and images
-        pdf_content, images = extract_pdf_contents(filepath)
+        # Display the entire PDF
+        display_entire_pdf(filepath)
 
-        pdf_text.delete("1.0", "end")  # Clear any previous text
-        pdf_text.insert("1.0", pdf_content)  # Insert the extracted text into the Text widget
+        print("PDF displayed")
 
-        # Display the images in the viewer
-        display_images(images)
-
-        print("Extracted text and images from the PDF")
-
-def extract_pdf_contents(pdf_filepath):
-    text = ""
-    images = []
+def display_entire_pdf(pdf_filepath):
     pdf_document = fitz.open(pdf_filepath)
+    pdf_text.delete("1.0", "end")  # Clear previous text
+
     for page_num in range(len(pdf_document)):
         page = pdf_document.load_page(page_num)
-        text += page.get_text()
-        images.append(page.get_pixmap())  # Append the pixmap to the list
-
-    return text, images
-
-def display_images(images):
-    for image in images:
-        image_pil = Image.frombytes("RGB", [image.width, image.height], image.samples)
-        image_tk = ImageTk.PhotoImage(image_pil)
-        pdf_images.append(image_tk)
-        image_label = tk.Label(viewer_frame, image=image_tk)
-        image_label.image = image_tk  # Keep a reference to the ImageTk object
-        image_label.grid(pady=10, padx=10)
+        text = page.get_text()
+        pdf_text.insert("end", text)  # Append the text
 
 def submit_question():
     question = question_entry.get()
@@ -95,7 +74,7 @@ upload_button = customtkinter.CTkButton(
 upload_button.grid(row=1, column=0, pady=30, padx=10)
 
 # Create a Text widget for the chatbox on the right
-chat_text = tk.Text(button_frame, wrap="word", width=50, height=20)
+chat_text = tk.Text(button_frame, wrap="word", width=80, height=10)  # Increased width
 chat_text.grid(row=2, column=0, pady=10, padx=10, sticky="nsew")
 
 # Create an entry field for questions
