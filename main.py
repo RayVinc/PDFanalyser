0a893f91c2e54a1bf31750249e7ca0d826159bb9
Dr. J. M. Ashfaque, MInstP, MAAT, AATQB <39216339+UKVeteran@users.noreply.github.com>
2023-11-04 06:39:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 import customtkinter
 from customtkinter import filedialog
 import tkinter as tk
-import fitz  # PyMuPDF
 from transformers import pipeline
+import fitz  # PyMuPDF
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
-root.geometry("1400x700")  # Increased the width and height
+root.geometry("1400x700")
 
-# Create a pipeline for question-answering using a pre-trained model
 qa_pipeline = pipeline("question-answering", model="distilbert-base-cased-distilled-squad")
 
-# Create a frame for the viewer on the left
 viewer_frame = customtkinter.CTkFrame(master=root)
 viewer_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
-# Create a Text widget to display PDF content on the left
-pdf_text = tk.Text(viewer_frame, wrap="none", width=80, height=20)  # Increased width
+pdf_text = tk.Text(viewer_frame, wrap="none", width=80, height=20)
 pdf_text.grid(row=0, column=0, pady=10, padx=10, sticky="nsew")
 
-# Create a frame for the title and buttons on the right
 button_frame = customtkinter.CTkFrame(master=root)
 button_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
 
@@ -33,26 +29,25 @@ def upload_pdf():
     if filepath:
         print(f"File path: {filepath}")
 
-        # Display the entire PDF
         display_entire_pdf(filepath)
 
         print("PDF displayed")
 
 def display_entire_pdf(pdf_filepath):
-    pdf_document = fitz.open(pdf_filepath)
     pdf_text.delete("1.0", "end")  # Clear previous text
+
+    pdf_document = fitz.open(pdf_filepath)
 
     for page_num in range(len(pdf_document)):
         page = pdf_document.load_page(page_num)
         text = page.get_text()
-        pdf_text.insert("end", text)  # Append the text
+        pdf_text.insert("end", text)
 
 def submit_question():
     question = question_entry.get()
     if question:
         chat_text.insert("end", f"User: {question}\n")
 
-        # Use the question-answering pipeline to answer the question
         answer = qa_pipeline(context=pdf_text.get("1.0", "end"), question=question)
 
         chat_text.insert("end", f"Analyzer: {answer['answer']}\n")
@@ -61,7 +56,6 @@ def submit_question():
 label = customtkinter.CTkLabel(master=button_frame, text="PDF Analyzer", font=("Arial", 32))
 label.grid(row=0, column=0, pady=12, padx=10)
 
-# Upload button
 upload_button = customtkinter.CTkButton(
     master=button_frame,
     text="Upload PDF",
@@ -73,19 +67,15 @@ upload_button = customtkinter.CTkButton(
 )
 upload_button.grid(row=1, column=0, pady=30, padx=10)
 
-# Create a Text widget for the chatbox on the right
-chat_text = tk.Text(button_frame, wrap="word", width=80, height=10)  # Increased width
+chat_text = tk.Text(button_frame, wrap="word", width=80, height=10)
 chat_text.grid(row=2, column=0, pady=10, padx=10, sticky="nsew")
 
-# Create an entry field for questions
 question_entry = tk.Entry(button_frame, width=40)
 question_entry.grid(row=3, column=0, pady=10, padx=10, sticky="nsew")
 
-# Create a button to submit questions
 submit_button = tk.Button(button_frame, text="Submit", command=submit_question)
 submit_button.grid(row=4, column=0, pady=10, padx=10, sticky="nsew")
 
-# Configure column and row weights to make widgets expand properly
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=1)
 root.grid_rowconfigure(0, weight=1)
